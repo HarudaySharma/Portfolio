5444efe6d1b6fba5ac3fc529e8bf5ecd47ebd389
HarudaySharma <harudayvasist@gmail.com>
2024-07-15 18:31:51 +0530

    added dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    darkMode: 'selector',
+    theme: {
+        extend: {
+            fontFamily: {
+                heading: ["Rubik Mono One", "sans-serif"],
+                text: ["Roboto Mono", "monospace"],
+            },
+            colors: {
+                "black": "var(--black-color)",
+                "white": "var(--white-color)",
+            }
+        },
+    },
+    plugins: [],
 }
